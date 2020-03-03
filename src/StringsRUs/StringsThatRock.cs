dda6aa79ebf0a8b62d3ef6f5b9d63cbd2f2ad422
ChrisEngstrom <tipher88@gmail.com>
2020-03-02 23:08:11 -0800

    Committing code with a PAT.

---
+++ src/StringsRUs/StringsThatRock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringsRUs
+{
+    public class StringsThatRock
+    {
+        public string GiveItToMe()
+        {
+            return ("Mickey Rocks Harder!");
+        }
+    }
+}
